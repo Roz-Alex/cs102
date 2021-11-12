786ba86b8f9ea20ee04afef2ad5e7e080f096a81
Roz-Alex <crea43953@gmail.com>
2021-11-12 15:56:11 +0300

    test

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -68,7 +68,7 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     # pass
 
 
-# print(multiplicative_inverse(7, 40))
+#print(multiplicative_inverse(4553, 8268))
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
