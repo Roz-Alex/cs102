488a621007d36283b5a9734d5da2a0de5247f62a
Roz-Alex <crea43953@gmail.com>
2022-01-14 23:21:34 +0300

    black fixed3

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -9,11 +9,11 @@ class Console(UI):
         super().__init__(life)
 
     def draw_borders(self, screen) -> None:
-        """ Отобразить рамку. """
+        """Отобразить рамку."""
         screen.border("|", "|", "-", "-", "+", "+", "+", "+")
 
     def draw_grid(self, screen) -> None:
-        """ Отобразить состояние клеток. """
+        """Отобразить состояние клеток."""
         grid = self.life.curr_generation
         for i in range(len(grid)):
             for j in range(len(grid[0])):
